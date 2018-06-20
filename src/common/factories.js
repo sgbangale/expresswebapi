afc44e7595289eaf39d4638cff183619f7c66e28
suraj bangale <sgbangale@gmail.com>
2018-06-21 00:30:18 +0530

    code for writing mongo schema to model.js

--- src/common/factories.js
+++ src/common/factories.js
@@ -2,28 +2,59 @@ var role = require('../models/role'),
     user = require('../models/user'),
     entity = require('../models/entity'),
     request = require('../models/requestPipeline'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    fs = require('fs');
+var sharedFunctions = {
+    entity__generateFile: function (entityData) {
+        var schemaData = JSON.stringify(entityData.entity_schema)
+            .replace(/"String"/g, 'String')
+            .replace(/"Mixed"/g, 'mongoose.Schema.Types.Mixed')
+            .replace(/""/g, ' ');
+        var data =
+            `var mongoose = require('mongoose');
+        mongoose.model('` + entityData.entity_code + `', mongoose.Schema(` + schemaData + `));
+        module.exports = mongoose.model('` + entityData.entity_code + `');
+        `;
+        console.log(data);
+        return data;
+    }
+};
 module.exports = {
 
     data__view: function (condition) {
-        var entity = mongoose.model("data");
+
+        var entity = require('../models/data');
+        entity.create({data_code:'sdasda asd asd sadas'})
         var viewPromise = new Promise(
             function (resolve, reject) {
-                entity.where(condition.filters).sort([
-                    ["'" + condition.sortField + "'", condition.sortOrder]
-                ]).count().exec().then((countData) => {
+                if (condition)
                     entity.where(condition.filters).sort([
                         ["'" + condition.sortField + "'", condition.sortOrder]
-                    ]).skip(condition.first).limit(condition.rows).exec(function (err, user) {
-                        if (err)
-                            reject(reason);
-                        else
-                            resolve({
-                                data: user,
-                                count: countData
-                            });
+                    ]).count().exec().then((countData) => {
+                        entity.where(condition.filters).sort([
+                            ["'" + condition.sortField + "'", condition.sortOrder]
+                        ]).skip(condition.first).limit(condition.rows).exec(function (err, user) {
+                            if (err)
+                                reject(reason);
+                            else
+                                resolve({
+                                    data: user,
+                                    count: countData
+                                });
+                        });
                     });
-                });
+                else {
+                    entity.find({}).exec(
+                        function (err, _data) {
+                            if (err)
+                                reject(reason);
+                            else
+                                resolve({
+                                    data: _data,
+                                });
+                        }
+                    );
+                }
             }
         );
         return viewPromise;
@@ -44,22 +75,17 @@ module.exports = {
                     if (err) {
                         reject(err);
                     } else {
-                        entitySchema = {};
-                        Object.keys(entityData.entity_schema).forEach(function (field) {
-                            var fieldDef = entityData.entity_schema[field];
-                            console.log(fieldDef);
-                            switch(fieldDef['type'])
-                            {
-                                case 'String':
-                                
-                                fieldDef['type'] = String;
-                                break;
-                            }
-                            entitySchema[field] = fieldDef;
-                           
+
+                        var fileContent = sharedFunctions.entity__generateFile(entityData);
+                        console.log(fileContent);
+                        fs.writeFile("src/models/business/" + entityData.entity_code + '.js', fileContent, (err) => {
+                            if (err) throw err;
+                            console.log(fileContent);
+                            console.log('The file has been saved!');
+                            resolve(fileContent);
                         });
-                        console.log(entitySchema);
-                        resolve(entityData);
+
+
                     }
 
                 });
@@ -70,7 +96,7 @@ module.exports = {
     },
     entity__view: function (condition) {
 
-        var entity = mongoose.model('Entity');
+        var entity = require('../models/data11');
         var viewPromise = new Promise(
             function (resolve, reject) {
                 if (condition)
